4b9fc63db16a802d1c1804c723742f3eb6a3fc28
Wilson-Pedro <wilsonpedroneto@gmail.com>
2024-08-06 21:31:05 -0300

    first commit

---
+++ loans-chanllenge/src/main/java/com/example/loans/LoansChanllengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansChanllengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansChanllengeApplication.class, args);
+	}
+
+}
